0f7760493db3121c5a4e62b651d871c69a548430
APHOTHAWATS <47128431+robinvansurat@users.noreply.github.com>
2022-09-17 12:52:36 +0700

    Update No1.js

--- No1.js
+++ No1.js
@@ -1,18 +1,22 @@
 function getKeyByValue(object, value) {
   return Object.keys(object).find((key) => object[key] === value);
 }
+function findMostValue(ary) {
+  let count = {};
+  let result = {};
+  q.forEach((element) => {
+    if (result[element]) {
+      count[element] += 1;
+    } else {
+      result[element] = element;
+      count[element] = 1;
+    }
+  });
+  const values = Object.values(count);
+  const max = Math.max(...values);
+  let key = getKeyByValue(count, max);
+  return { result: key, count: max };
+}
+
 let q = [6, -1, 6, 3, 3, 6, 5, 6, 6, 7];
-let count = {};
-let result = {};
-q.forEach((element) => {
-  if (result[element]) {
-    count[element] += 1;
-  } else {
-    result[element] = element;
-    count[element] = 1;
-  }
-});
-const values = Object.values(count);
-const max = Math.max(...values);
-let key = getKeyByValue(count, max);
-console.log({ result: key, count: max });
+console.log(findMostValue(q));
